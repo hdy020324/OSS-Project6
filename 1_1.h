349ab4b1eee7f6d554485730ce254f56d8ad2636
KoahOwO <lovekm0121@naver.com>
2021-12-07 14:49:59 +0900

    add 1_1

---
+++ 1_1.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void P1_1sel();
+void P1_1formula(int);
+void avrspeed();
+void avrvelocity();
+void avracceleration();
+void momentum();
+void impulse();
+void joule();
+void power();
+void kineticenergy();
+void potentialenergy();
