8c1b016689b04da48aee1ef1d1e89ebb2355498d
Koah_OwO <lovekm0121@gmail.com>
2021-12-11 18:21:13 +0900

    2_1, 2_3 구분 후 메인 부분과 병합함

--- 2_13.h
+++ 2_13.h
@@ -1,4 +1,5 @@
 #pragma once
+
 double Vertical_upward_movement(int cho);
 double parabolic_motion(int cho);
 double inertial_force(int cho);
@@ -7,6 +8,5 @@ double wave_of_principle(int cho);
 double wave_rbdi(int cho);
 double doppler_I(int cho);
 double mirror_lens(int cho);
-
-void sub2_unit1();
-void sub2_unit3();
+void P2_1sel();
+void P2_3sel();
